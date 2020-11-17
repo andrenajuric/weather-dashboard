898faf7f4d46040117d0df237a029731adfc139d
Andrena Juric <ajuric.uw@gmail.com>
2020-11-16 17:36:29 -0800

    created assets folder, moved js and css files - trying to fix 404 error

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+var cityInput = document.querySelector("#city-input");
+var searchButton = document.querySelector("#searchBtn");
+
+function citySearch(event) {
+    event.preventDefault();
+
+    var city = cityInput.val().toLowerCast();
+}
+
+function getWeather(city) {
+
+    var APIKey = "cf0e69cbcb190a2cd7bb4cb1feb02364";
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + api;
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response)
+    });
+
+}
